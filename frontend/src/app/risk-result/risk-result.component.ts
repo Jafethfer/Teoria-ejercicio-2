a506827ddb4f7967fa9028056eee9cd0ee1c80e9
Alberto Medina <luisitomedina95@gmail.com>
2021-11-26 15:52:22 -0600

    RiskResult component

--- frontend/src/app/risk-result/risk-result.component.ts
+++ frontend/src/app/risk-result/risk-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-risk-result',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./risk-result.component.css']
 })
 export class RiskResultComponent implements OnInit {
+
+  faHeart = faHeart;
 
   constructor() { }
 

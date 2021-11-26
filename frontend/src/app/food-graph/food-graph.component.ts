c1814fa1bf0c72252d569c970b4a7fd96249f3df
Alberto Medina <luisitomedina95@gmail.com>
2021-11-26 17:11:21 -0600

    FoodGraph component

--- frontend/src/app/food-graph/food-graph.component.ts
+++ frontend/src/app/food-graph/food-graph.component.ts
@@ -1,4 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  // Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+} from 'chart.js';
 
 @Component({
   selector: 'app-food-graph',
@@ -6,10 +32,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food-graph.component.css']
 })
 export class FoodGraphComponent implements OnInit {
-
-  constructor() { }
+  
+  labels = ["Queso","Huevo","Higado","Pollo","Sardinas"];
 
   ngOnInit(): void {
+
+    Chart.register(
+      ArcElement,
+      LineElement,
+      BarElement,
+      PointElement,
+      BarController,
+      BubbleController,
+      DoughnutController,
+      LineController,
+      PieController,
+      PolarAreaController,
+      RadarController,
+      ScatterController,
+      CategoryScale,
+      LinearScale,
+      LogarithmicScale,
+      RadialLinearScale,
+      TimeScale,
+      TimeSeriesScale,
+      Filler,
+      Legend,
+      Title,
+      Tooltip
+    );
+    
+    const data = {
+      labels: this.labels,
+      datasets: [{
+          label: 'Contenido de colesterol',
+          backgroundColor: 'rgb(25, 99, 132)',
+          borderColor: 'rgb(25, 99, 132)',
+          data: [5,3,2,4,1],
+      }]
+  };
+
+  const myChart = new Chart("myChar", {
+    type: 'bar',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+                display: false
+            },
+        }
+    },
+}
+  );
   }
 
 }

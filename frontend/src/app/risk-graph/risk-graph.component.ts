e9d3b8f3ddf041529462825a4aaf887eafe3507c
Jafethfer <brando.fernandez@unah.hn>
2021-11-27 16:18:34 -0600

    Grafico de riesgos

--- frontend/src/app/risk-graph/risk-graph.component.ts
+++ frontend/src/app/risk-graph/risk-graph.component.ts
@@ -1,4 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  // Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+} from 'chart.js';
 
 @Component({
   selector: 'app-risk-graph',
@@ -7,9 +33,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RiskGraphComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+    Chart.register(
+      ArcElement,
+      LineElement,
+      BarElement,
+      PointElement,
+      BarController,
+      BubbleController,
+      DoughnutController,
+      LineController,
+      PieController,
+      PolarAreaController,
+      RadarController,
+      ScatterController,
+      CategoryScale,
+      LinearScale,
+      LogarithmicScale,
+      RadialLinearScale,
+      TimeScale,
+      TimeSeriesScale,
+      Filler,
+      Legend,
+      Title,
+      Tooltip
+    );
+
+    const data = {
+      labels: [50,60,70,80],
+      datasets: [{
+        label: 'Gráfico de riesgos hasta los 80 años',
+        data: [14,20,25,20],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }]
+    };
+
+    const myChart = new Chart('riskChart',
+    {
+      type: 'line',
+      data:data,
+    })
   }
+
+
 
 }

72f20156bb32c0fccf6a515d142853451e78465e
Jafethfer <brando.fernandez@unah.hn>
2021-11-26 19:51:45 -0600

    Actualizacion de componente raiz y columnas de graficos

--- frontend/src/app/formulario/formulario.component.ts
+++ frontend/src/app/formulario/formulario.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {faQuestionCircle} from '@fortawesome/free-regular-svg-icons'
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
-
-  constructor() { }
+  faQuestionCircle = faQuestionCircle
+  constructor(private modalService:NgbModal) { }
 
   ngOnInit(): void {
   }
 
+  helpModal(longContent:any){
+    this.modalService.open(longContent,{scrollable:true})
+  }
 }
